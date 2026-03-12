1b20fbef4e5583e83b77e1c183de766dd9f63861
Your Name <you@example.com>
2026-03-12 03:21:23 +0100

    sssd

--- check_admin_user.js
+++ check_admin_user.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 // Instrucciones: Cambia estos valores para probar el proyecto que usa Vercel
-const SB_URL = 'https://fmeapqgydztrdurvbfdv.supabase.co'; // La URL que me pasaste antes de Vercel
-const SB_KEY = 'TU_ANON_KEY_DE_ESE_PROYECTO';
+const SB_URL = 'https://jhgbhxlxswduimlkuqms.supabase.co'; // La URL que me pasaste antes de Vercel
+const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoZ2JoeGx4c3dkdWltbGt1cW1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNDI1NzEsImV4cCI6MjA4NzgxODU3MX0.PaDmQmB-g1lXRTidIkKc3SSZq8-e6vpg2FJ3u4ASRAQ';
 
 const supabase = createClient(SB_URL, SB_KEY);
 
